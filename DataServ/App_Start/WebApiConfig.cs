4389b319c048b59f1e00458093f2326984810cac
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2019-11-27 22:03:28 +0200

    Update WebApiConfig.cs

--- DataServ/App_Start/WebApiConfig.cs
+++ DataServ/App_Start/WebApiConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,13 +13,23 @@ namespace DataServ
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
             // Web API routes
-            config.MapHttpAttributeRoutes();
+        
+        
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            GlobalConfiguration.Configure(configu =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                 name: "DefaultApi",
+                 routeTemplate: "api/{controller}/{action}/{id}",
+                 defaults: new { id = RouteParameter.Optional }
+                 );
+            });
         }
+
+
     }
-}
+
+       
+    }
