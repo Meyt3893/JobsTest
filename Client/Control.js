25cdebb7f941539a9a584f57136feaa357797151
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2019-12-01 20:02:42 +0200

    Update Control.js

--- Client/Control.js
+++ Client/Control.js
@@ -1,12 +1,34 @@
-﻿
+
 var myapp = angular.module("myapp", ['ngRoute']);
 
 myapp.controller("homeController", ['$scope', '$http',function ($scope, $http) {
  
-   
+    $scope.hidethis = true;
+    $scope.complete = function (input) {
+        $scope.hidethis = false;
+  
+        $http({
+            method: "GET",
+            url: "http://localhost:59836/api/Jobs/autocomp/" + input
+
+        }).then(function Success(response) {
+
+            $scope.complist = JSON.parse(response.data);
+            console.log(response.data);
+
+        }, function Error(response) {
     
+        });
+
+    }
+    $scope.fillTextbox = function (string) {
+        $scope.inSearch = string;
+        $scope.hidethis = true;
+    }  
+ 
     $scope.search = function (inSearch) {
         $scope.inSearch = inSearch;
+        $scope.hidethis = true;
         $http({
             method: "GET",
             url: "http://localhost:59836/api/Jobs/searchjobs/" + $scope.inSearch
@@ -14,12 +36,14 @@ myapp.controller("homeController", ['$scope', '$http',function ($scope, $http) {
         }).then(function Success(response) {
 
             $scope.jobs = JSON.parse(response.data);       
+            console.log(response.data);
 
         }, function Error(response) {
                 alert(response.statusText);
         });
 
     };
+
 
     
    
